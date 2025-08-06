c00cfdd0d74c1d198b6712d8114331631e985e58
Brayan_Dev <154345685+InoBC@users.noreply.github.com>
2025-08-06 12:51:55 -0600

    COMMIT #2
    COMMIT #2

---
+++ js/auth.js
@@ -0,0 +1,49 @@
+const SUPABASE_URL = "https://idkrqgauvrikltsrzzgh.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlka3JxZ2F1dnJpa2x0c3J6emdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1MDQ4OTQsImV4cCI6MjA3MDA4MDg5NH0.TAkxZu6MT_epcNpneIYM7JKzumJ3n0lrEvHb7TmycxU";
+
+
+const client = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+function toggleForms() {
+  const loginForm = document.getElementById("login-form");
+  const registerForm = document.getElementById("register-form");
+
+  loginForm.style.display = loginForm.style.display === "none" ? "block" : "none";
+  registerForm.style.display = registerForm.style.display === "none" ? "block" : "none";
+}
+
+async function register() {
+  const email = document.getElementById("reg-email").value;
+  const password = document.getElementById("reg-password").value;
+
+  const { data, error } = await client.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    alert("Error: " + error.message);
+  } else {
+    alert("Registro exitoso.");
+    toggleForms();
+  }
+}
+
+async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } = await client.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    alert("Error: " + error.message);
+  } else {
+    alert("Sesión iniciada.");
+    localStorage.setItem("token", data.session.access_token);
+
+     window.location.href = "dashboard.html"; //Registro Estudiantes
+  }
+}
